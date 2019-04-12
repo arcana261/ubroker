2e79839041737cd8ee7a0b5149e1911930c5bcf1
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-04-12 15:25:58 +0430

    internal/broker: Implement basic Delivery()

--- internal/broker/core.go
+++ internal/broker/core.go
@@ -13,16 +13,35 @@ import (
 // we requeue an unacknowledged/un-re-queued message
 // automatically.
 func New(ttl time.Duration) ubroker.Broker {
-	return &core{}
+	return &core{
+		sequenceNumber: -1,
+		mainChannel:    make(chan ubroker.Delivery),
+		ttl:            ttl,
+		closed:         false,
+	}
 }
 
 type core struct {
 	// TODO: add required fields
+	sequenceNumber int
+	mainChannel    chan ubroker.Delivery
+
+	ttl time.Duration
+
+	closed bool
 }
 
 func (c *core) Delivery(ctx context.Context) (<-chan ubroker.Delivery, error) {
-	// TODO:‌ implement me
-	return nil, errors.Wrap(ubroker.ErrUnimplemented, "method Delivery is not implemented")
+	// check if context has error
+	if err := filterContextError(ctx); err != nil {
+		return nil, ctx.Err()
+	}
+	// checking the broker
+	if c.closed {
+		return nil, errors.Wrap(ubroker.ErrClosed, "The broker is closed.")
+	}
+
+	return c.mainChannel, nil
 }
 
 func (c *core) Acknowledge(ctx context.Context, id int) error {
@@ -42,7 +61,9 @@ func (c *core) Publish(ctx context.Context, message ubroker.Message) error {
 
 func (c *core) Close() error {
 	// TODO:‌ implement me
-	return errors.Wrap(ubroker.ErrUnimplemented, "method Close is not implemented")
+	c.closed = true
+
+	return nil
 }
 
 // Checks if the context has an error, returns true if the deadline has exceeded, or context had canceled
