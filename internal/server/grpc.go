acd39b6d39de8d7cccbc1550e6202753c6f45e69
mahtabfarrokh <mahtab.farrokh@yahoo.com>
2019-05-24 12:06:08 +0430

    conflict problem :(

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -2,10 +2,9 @@ package server
 
 import (
 	"context"
-	"io"
 
-	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/arcana261/ubroker/pkg/ubroker"
+	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -19,63 +18,19 @@ func NewGRPC(broker ubroker.Broker) ubroker.BrokerServer {
 		broker: broker,
 	}
 }
+
 func (s *grpcServicer) Fetch(stream ubroker.Broker_FetchServer) error {
-	delivery, err := s.broker.Delivery(stream.Context())
-	if err != nil {
-		return s.ReturnError(err)
-	}
-	for {
-		_, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			println("heeeeeeeeeeeeeeeeeeeeeer!")
-			return s.ReturnError(err)
-		}
-		delivered := <-delivery
-		if delivered == nil {
-			return status.Error(codes.Unavailable, "Unavailable")
-		}
-		err = stream.Send(delivered)
-		if err != nil {
-			return s.ReturnError(err)
-		}
-	}
-	//return status.Error(codes.OK, "OK")
+	return status.Error(codes.Unimplemented, "not implemented")
 }
 
 func (s *grpcServicer) Acknowledge(ctx context.Context, request *ubroker.AcknowledgeRequest) (*empty.Empty, error) {
-	err := s.broker.Acknowledge(ctx, request.Id)
-	if err != nil {
-		return &empty.Empty{}, s.ReturnError(err)
-	}
-	return &empty.Empty{}, status.Error(codes.OK, "OK")
+	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
 }
 
 func (s *grpcServicer) ReQueue(ctx context.Context, request *ubroker.ReQueueRequest) (*empty.Empty, error) {
-	err := s.broker.ReQueue(ctx, request.Id)
-	if err != nil {
-		return &empty.Empty{}, s.ReturnError(err)
-	}
-	return &empty.Empty{}, status.Error(codes.OK, "OK")
+	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
 }
 
 func (s *grpcServicer) Publish(ctx context.Context, request *ubroker.Message) (*empty.Empty, error) {
-	err := s.broker.Publish(ctx, request)
-	if err != nil {
-		return &empty.Empty{}, s.ReturnError(err)
-	}
-	return &empty.Empty{}, status.Error(codes.OK, "OK")
+	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
 }
-func (s *grpcServicer) ReturnError(err error) error {
-	if err == ubroker.ErrClosed {
-		return status.Error(codes.Unavailable, "Unavailable")
-	}
-	if err == ubroker.ErrInvalidID {
-		return status.Error(codes.InvalidArgument, "InvalidID")
-	}
-	return nil
-}
-
-
