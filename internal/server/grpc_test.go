2ebdb18dba129d1f69884e1e83e7c7a167a81a09
sneyes <sneyes@yahoo.com>
2019-05-25 21:40:23 +0430

    Testing the other way around

--- internal/server/grpc_test.go
+++ internal/server/grpc_test.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/stretchr/testify/mock"
 
-	"github.com/sneyes/ubroker/internal/server"
-	"github.com/sneyes/ubroker/pkg/ubroker"
+	"github.com/arcana261/ubroker/internal/server"
+	"github.com/arcana261/ubroker/pkg/ubroker"
 	"github.com/phayes/freeport"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
