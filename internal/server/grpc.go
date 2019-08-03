26e502e9ce161f7ad41c044536ccb8847b1d262b
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-08-04 03:39:19 +0430

    internal/server: Implement grpc services

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -2,11 +2,11 @@ package server
 
 import (
 	"context"
-
-	"github.com/arcana261/ubroker/pkg/ubroker"
-	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/meshkati/ubroker/pkg/ubroker"
 )
 
 type grpcServicer struct {
@@ -20,17 +20,61 @@ func NewGRPC(broker ubroker.Broker) ubroker.BrokerServer {
 }
 
 func (s *grpcServicer) Fetch(stream ubroker.Broker_FetchServer) error {
-	return status.Error(codes.Unimplemented, "not implemented")
+	deliveryChan, err := s.broker.Delivery(stream.Context())
+	if err != nil {
+		return convertError(err)
+	}
+	// it's stream, so the count of incoming messages is unknown
+	for {
+		_, err := stream.Recv()
+		if err != nil {
+			return convertError(err)
+		}
+
+		message := <- deliveryChan
+		if message == nil {
+			return convertError(ubroker.ErrClosed)
+		}
+
+		err = stream.Send(message)
+		if err != nil {
+			return convertError(err)
+		}
+
+	}
 }
 
 func (s *grpcServicer) Acknowledge(ctx context.Context, request *ubroker.AcknowledgeRequest) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	err := s.broker.Acknowledge(ctx, request.GetId())
+	serviceError := convertError(err)
+	return &empty.Empty{}, serviceError
 }
 
 func (s *grpcServicer) ReQueue(ctx context.Context, request *ubroker.ReQueueRequest) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	err := s.broker.ReQueue(ctx, request.GetId())
+	serviceError := convertError(err)
+	return &empty.Empty{}, serviceError
 }
 
 func (s *grpcServicer) Publish(ctx context.Context, request *ubroker.Message) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	err := s.broker.Publish(ctx, request)
+	serviceError := convertError(err)
+	return &empty.Empty{}, serviceError
+}
+
+
+func convertError(err error) error {
+	switch err {
+	case ubroker.ErrClosed:
+		return status.Error(codes.Unavailable, "Service unavailable")
+	case ubroker.ErrInvalidID:
+		return status.Error(codes.InvalidArgument, "Invalid argument")
+	case ubroker.ErrUnimplemented:
+		return status.Error(codes.Unimplemented, "Service unimplemented")
+	default:
+		if err == nil {
+			return nil
+		}
+		return status.Error(codes.Unknown, "Unknown service error!")
+	}
 }
