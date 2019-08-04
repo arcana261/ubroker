a9edc398bf577147f21632dba093e5e314351321
MohammadHossein <mhhessar1376@gmail.com>
2019-08-05 00:06:38 +0430

    grcp phase2

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -2,17 +2,29 @@ package server
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/arcana261/ubroker/pkg/ubroker"
 	"github.com/golang/protobuf/ptypes/empty"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type grpcServicer struct {
 	broker ubroker.Broker
 }
 
+func toServiceError(err error) error {
+	switch err {
+	case nil:
+		return nil
+	case ubroker.ErrClosed:
+		return status.Error(codes.Unavailable, "ErrClosed")
+	case ubroker.ErrInvalidID:
+		return status.Error(codes.InvalidArgument, "ErrInvalidID")
+	default:
+		return status.Error(codes.Unknown, "Unknown!")
+	}
+}
 func NewGRPC(broker ubroker.Broker) ubroker.BrokerServer {
 	return &grpcServicer{
 		broker: broker,
@@ -20,17 +32,37 @@ func NewGRPC(broker ubroker.Broker) ubroker.BrokerServer {
 }
 
 func (s *grpcServicer) Fetch(stream ubroker.Broker_FetchServer) error {
-	return status.Error(codes.Unimplemented, "not implemented")
+	deliveryChannel, err := s.broker.Delivery(stream.Context())
+	if err != nil {
+		return toServiceError(err)
+	}
+
+	for ; ; {
+		_, err := stream.Recv()
+		if err != nil {
+			return toServiceError(err)
+		}
+
+		msg, ok := <-deliveryChannel;
+		if ok {
+			err := stream.Send(msg)
+			if err != nil {
+				return toServiceError(err)
+			}
+			return nil
+		}
+		return toServiceError(ubroker.ErrClosed)
+	}
 }
 
 func (s *grpcServicer) Acknowledge(ctx context.Context, request *ubroker.AcknowledgeRequest) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	return &empty.Empty{}, toServiceError(s.broker.Acknowledge(ctx,request.GetId()))
 }
 
 func (s *grpcServicer) ReQueue(ctx context.Context, request *ubroker.ReQueueRequest) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	return &empty.Empty{}, toServiceError(s.broker.ReQueue(ctx,request.GetId()))
 }
 
 func (s *grpcServicer) Publish(ctx context.Context, request *ubroker.Message) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	return &empty.Empty{}, toServiceError(s.broker.Publish(ctx,request.GetId()))
 }
